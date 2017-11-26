1471c2bfa2a112cb9fea5345442c00ce316b1a75
karu_uoc <arunsocs@gmail.com>
2017-11-26 10:03:02 +0100

    Skelton  Skelton of the project

---
+++ SeqtoSeq.py
@@ -0,0 +1,36 @@
+from __future__ import unicode_literals, print_function, division
+from io import open
+import unicodedata
+import string
+import re
+import random
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch import optim
+import torch.nn.functional as F
+
+use_cuda = torch.cuda.is_available()
+
+SOW_token = 0
+EOW_token = 1
+
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2  # Count SOS and EOS
+
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
